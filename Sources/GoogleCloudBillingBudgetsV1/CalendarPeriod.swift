72505a63f2abc76a2cb157ba6f51d59ec4db13ba
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudBillingBudgetsV1/CalendarPeriod.swift
+++ Sources/GoogleCloudBillingBudgetsV1/CalendarPeriod.swift
@@ -79,7 +79,7 @@ public enum CalendarPeriod: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:CalendarPeriod/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "CALENDAR_PERIOD_UNSPECIFIED": self = .unspecified
@@ -92,7 +92,7 @@ public enum CalendarPeriod: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:CalendarPeriod/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
--- Sources/GoogleCloudBillingBudgetsV1/ThresholdRule.swift
+++ Sources/GoogleCloudBillingBudgetsV1/ThresholdRule.swift
@@ -128,7 +128,7 @@ public struct ThresholdRule: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:Basis/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "BASIS_UNSPECIFIED": self = .unspecified
@@ -140,7 +140,7 @@ public struct ThresholdRule: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:Basis/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
