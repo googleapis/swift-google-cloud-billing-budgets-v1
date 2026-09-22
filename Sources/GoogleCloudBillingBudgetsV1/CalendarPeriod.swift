28b49443e2caf587e4198585dc517926a1375a27
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudBillingBudgetsV1/CalendarPeriod.swift
+++ Sources/GoogleCloudBillingBudgetsV1/CalendarPeriod.swift
@@ -20,6 +20,12 @@ import Foundation
 /// has a canonical start. Grammatically, "the start of the current
 /// `CalendarPeriod`". All calendar times begin at 12 AM US and Canadian
 /// Pacific Time (UTC-8).
+///
+/// - Note: Adding cases to this enumeration is not considered a breaking change.
+///   Always include an `@unknown default:` case when switching over this type.
+///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+///   expecting specific values to remain unparsed; future releases may promote
+///   them to named cases.
 public enum CalendarPeriod: Codable, Equatable, Sendable {
   /// Calendar period is unset. This is the default if the budget is for a
   /// custom time period (CustomPeriod).
@@ -34,15 +40,21 @@ public enum CalendarPeriod: Codable, Equatable, Sendable {
   case year
   /// Encodes an unknown integer value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific integer values in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownIntValue(Int)
   /// Encodes an unknown string value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific string literals in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownStringValue(String)
 
   public init() {
